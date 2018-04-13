86dacd9bbb80897f7c2e17f943aaa1fa86511788
Schtoo <mgqobhoz@gmail.com>
2018-04-13 14:25:46 +0200

    added more different scenarios

--- tests/grandTotal.test.js
+++ tests/grandTotal.test.js
@@ -1,21 +1,22 @@
-var depts = [
-  {name : 'toiletries', total: 5007},
-  {name : 'groceries', total: 11089},
-  {name: 'pharmacy', total: 7321},
-  {name: 'bakery', total: 10073},
-  {name: 'butchery', total: 9053}];
+  var depts = [
+    {name : 'toiletries', total: 5007},
+    {name : 'groceries', total: 11089},
+    {name: 'pharmacy', total: 7321},
+    {name: 'bakery', total: 10073},
+    {name: 'butchery', total: 9053}];
 
 describe('grandTotal', function(){
   it('should give you a total of all the departmemts', function(){
     assert.deepEqual(42543, grandTotal(depts));
+
   });
   it('should give you a total as a number & not a string', function(){
     assert.notDeepEqual('', grandTotal(depts));
   });
-  it('Expected 12370 to equal 42543', function(){
-    assert.isBelow(12370, grandTotal(depts))
+  it('Should not give you a number less than 42543', function(){
+    assert.isBelow(42543, grandTotal(depts))
   });
-  it('Expected 67343 to equal 42543', function(){
-    assert.isAtMost(67343, grandTotal(depts))
+  it('Should give you a number morethan the total', function(){
+    assert.isAtMost(42543, grandTotal(depts))
   });
 });
